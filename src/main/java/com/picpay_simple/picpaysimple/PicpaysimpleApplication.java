052a0127b380a51f6f59bb6e879f373e5021ae21
Pedro <peuhenry2018@gmail.com>
2025-02-06 12:23:21 -0300

    Initial Commits ✨

---
+++ src/main/java/com/picpay_simple/picpaysimple/PicpaysimpleApplication.java
@@ -0,0 +1,13 @@
+package com.picpay_simple.picpaysimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpay_simple/picpaysimple/PicpaysimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay_simple.picpaysimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
