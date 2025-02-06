c28c63ec166ddfeceeb2391996368480bac9c0fb
Pedro <peuhenry2018@gmail.com>
2025-02-06 13:42:31 -0300

    Create User Class ✅

---
+++ src/main/java/com/picpay_simple/picpaysimple/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.picpay_simple.picpaysimple.repositories;
+
+import com.picpay_simple.picpaysimple.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserByID(Long id);
+}
